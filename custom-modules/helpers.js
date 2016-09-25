f2676a709f20ed6d44f371259ed600fe9f2488b4
Vytautas Brundza <kiaurutis@gmail.com>
2016-09-25 13:13:08 +0300

    Part 4.1: Helper functions

---
+++ custom-modules/helpers.js
@@ -0,0 +1,30 @@
+"use strict"
+
+module.exports = {
+//---  HELPER FUNCTIONS   ---
+
+	// Two-dimensional vector constructor
+	V2: function(x,y) {
+		return {x:x,y:y};
+	},
+	
+	// Find object in array by its key value
+	objectFindByKey: function(array, key, value) {
+		for (var i = 0; i < array.length; i++) {
+			if (array[i] && array[i][key] == value) {
+				return array[i];
+			}
+		}
+		return null;
+	},
+	
+	// Find objects index in array by its key value
+	indexFindByKey: function(array, key, value) {
+		for (var i = 0; i < array.length; i++) {
+			if (array[i] && array[i][key] == value) {
+				return i;
+			}
+		}
+		return null;
+	}
+};
